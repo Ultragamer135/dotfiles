3505fd8fefced0d4dc08d7166aa5cb62d55b0047
Ren <elinero27@gmail.com>
2025-05-07 19:41:04 -0400

    Added some fish plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-function gitignore; curl -sL https://www.gitignore.io/api/; end
 abbr --add :q exit
 # pnpm
 set -gx PNPM_HOME "/home/elin/.local/share/pnpm"
@@ -6,3 +5,8 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+function gitignore; curl -sL https://www.gitignore.io/api/; end
+function mkcd
+    mkdir -p $argv[1] && cd $argv[1]
+end
