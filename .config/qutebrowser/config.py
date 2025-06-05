c544af4c05d9084081fecd61a923d79dbea180af
Ren Nero <elinero27@gmail.com>
2025-06-05 00:54:42 -0400

    Qutebrowser editor fix

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,6 +2,7 @@
 c.auto_save.session = True
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.preferred_color_scheme = "dark"
+c.editor.command = ["kitty", "nvim", "{file}"];
 # Keybinds
 ## Tabs
 config.bind('<Ctrl+Shift+Left>', 'tab-prev')
@@ -9,16 +10,7 @@ config.bind('<Ctrl+Shift+Right>', 'tab-next')
 config.bind('<Ctrl+t>', 'cmd-set-text -s :open -t')
 config.bind('<Ctrl+Shift+t>', 'open -t')
 config.bind('<Ctrl+f>', 'cmd-set-text /')
-## WASD
-config.bind('w', 'scroll up')
-config.bind('a', 'scroll left')
-config.bind('s', 'scroll down')
-config.bind('d', 'scroll right')
-## Visual mode WASD
-config.bind('w', 'move-to-prev-line', mode='caret')
-config.bind('a', 'move-to-prev-char', mode='caret')
-config.bind('s', 'move-to-next-line', mode='caret')
-config.bind('d', 'move-to-next-char', mode='caret')
+config.bind('a', 'mode-enter insert')
 ## Visual mode arrows
 config.bind('Up', 'move-to-prev-line', mode='caret')
 config.bind('Down', 'move-to-next-line', mode='caret')
