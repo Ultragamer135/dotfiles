fef7d3661d16cf5230de4cf679ce6fbacf78b46c
Ren Nero <elinero27@gmail.com>
2026-06-12 20:13:43 -0400

    J

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
   {
     "stevearc/oil.nvim",
     opts = {},
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open file path in Oil" },
+    },
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   },
 }
