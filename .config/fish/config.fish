55442a270a294951f784127c2cce13620e9eb653
Ren Nero <elinero27@gmail.com>
2026-06-12 20:34:15 -0400

    Made it easier to exit terminal mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,9 @@ function fish_user_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
 end
+function fish_greeting
+    echo "Welcome \$HOME, Ren!"
+end
 
 # Vars
 set -gx EDITOR nvim
@@ -13,20 +16,33 @@ set -gx MANPAGER 'nvim +Man!'
 # Abbreviations
 abbr --add :q exit # Yayy vim
 function :q! # Kill session and exit
-    tmux kill-session; exit
+    tmux kill-session
+    exit
 end
 abbr --add pacman sudo pacman # Because I'm tired of typing 'sudo'
-function '.'; nvim "+Oil $argv"; end
+function '.'
+    nvim "+Oil $argv"
+end
 abbr --add s kitten ssh
 abbr --add !! 'clear && exec fish'
 which paru &>/dev/null && abbr yay paru
 
 # Functions
-function gitignore; curl -sL https://www.gitignore.io/api/$argv; end # Gitignore generator
-function wttr; curl "https://wttr.in/$argv?1F"; end # Weather
-function cheat; curl cheat.sh/(string join + $argv) | bat; end # Cheat sheet!
-which cht.sh &>/dev/null && function cheat; cht.sh $argv | bat; end
-function mkcd; mkdir -p $argv[1] && cd $argv[1]; end # Make a directory and enter it
+function gitignore
+    curl -sL https://www.gitignore.io/api/$argv
+end # Gitignore generator
+function wttr
+    curl "https://wttr.in/$argv?1F"
+end # Weather
+function cheat
+    curl cheat.sh/(string join + $argv) | bat
+end # Cheat sheet!
+which cht.sh &>/dev/null && function cheat
+    cht.sh $argv | bat
+end
+function mkcd
+    mkdir -p $argv[1] && cd $argv[1]
+end # Make a directory and enter it
 
 # Go
 fish_add_path /usr/local/go/bin
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
